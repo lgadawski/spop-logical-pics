97cfd2471120d10e84bc05860de37b3907fa2743
Lukasz Gadawski <l.gadawski@gmail.com>
2015-06-08 23:42:03 +0200

    one numbers in lines

--- logical-pics.hs
+++ logical-pics.hs
@@ -1,24 +1,65 @@
+import System.Environment
+import System.IO
+
 import Data.Matrix
+
 import qualified Data.Vector
 import qualified Debug.Trace 
 import qualified Data.Vector.Generic
 
 main = do
-	print (findAllHash 
-			[2, 4, 3, 1]
-			[2, 3, 2, 3])
-			--[[1], [4], [3], [2]]
-			--[[2], [3], [2], [3]])
+	putStrLn "Please type file name with board data:"  
+	
+	filename <- getLine  
+	
+	putStrLn ("Opening: " ++ filename)
+
+	handle <- openFile filename ReadMode
+	rowList <- hGetLine handle
+	colList <- hGetLine handle 
+	let rows = read rowList::[Int]
+	let cols = read colList::[Int]
+	
+	putStrLn ("Rows: " ++ rowList)
+	putStrLn ("Columns: " ++ colList)
+	
+	print (findAllHash rows cols)
 
 -- Funkcja inicująca metodę znajdująca wszystkie zamalowane punkty
 findAllHash ys xs =  findAllHash' ys xs (matrix (length ys) (length xs) $ \(_,_) -> 0)
 
 findAllHash' :: [Int] -> [Int] -> Matrix Int -> Matrix Int
 findAllHash' ys xs result | isAllBoardFilled result = result
-						  | otherwise = (Debug.Trace.trace (prettyMatrix result) 
-								(findAllHash' ys xs
-									(fill2IfMaxVals ys 1 xs 1
-										(fillIntersections ys 1 (length ys) xs 1 (length xs) (fillAllCols 1 xs (fillAllRows 1 ys result))))))
+						  | otherwise = (Debug.Trace.trace (prettyMatrix result) 					
+									(findAllHash' ys xs
+										(fillPossibilities ys 1 xs 1 
+											(fill2IfMaxVals ys 1 xs 1
+												(fillIntersections ys 1 (length ys) xs 1 (length xs) (fillAllCols 1 xs (fillAllRows 1 ys result)))))))
+
+fillPossibilities :: [Int] -> Int -> [Int] -> Int -> Matrix Int -> Matrix Int
+fillPossibilities [] _ [] _ result = result
+fillPossibilities [] rowIndex (x:xs) colIndex result | countZeroVals (Data.Vector.Generic.toList (getCol colIndex result)) == x 
+															 && countOneVals (Data.Vector.Generic.toList (getCol colIndex result)) == 0
+														= fillOnesInZerosInCol (Data.Vector.Generic.toList (getCol colIndex result)) 1 colIndex result
+													 | countZeroValsAndNear1s (Data.Vector.Generic.toList (getCol colIndex result)) 0 == x 
+													 	= fillOnesInZerosInCol (Data.Vector.Generic.toList (getCol colIndex result)) 1 colIndex result
+													 | otherwise = fillPossibilities [] rowIndex xs (colIndex+1) result
+fillPossibilities (y:ys) rowIndex xs colIndex result | countZeroVals (Data.Vector.Generic.toList (getRow rowIndex result)) == y 
+															 && countOneVals (Data.Vector.Generic.toList (getRow rowIndex result)) == 0
+														= fillOnesInZerosInRow (Data.Vector.Generic.toList (getRow rowIndex result)) 1 rowIndex result
+													 | countZeroValsAndNear1s (Data.Vector.Generic.toList (getRow rowIndex result)) 0 == y 
+												 		= fillOnesInZerosInRow (Data.Vector.Generic.toList (getRow rowIndex result)) 1 rowIndex result
+													 | otherwise = fillPossibilities ys (rowIndex+1) xs colIndex result
+
+fillOnesInZerosInCol :: [Int] -> Int -> Int -> Matrix Int -> Matrix Int
+fillOnesInZerosInCol [] _ _ result = result
+fillOnesInZerosInCol (x:xs) colIndex counter result | x == 0 = fillOnesInZerosInCol xs colIndex (counter+1) (setElem 1 (counter, colIndex) result)
+													| otherwise = fillOnesInZerosInCol xs colIndex (counter+1) result
+
+fillOnesInZerosInRow :: [Int] -> Int -> Int -> Matrix Int -> Matrix Int
+fillOnesInZerosInRow [] _ _ result = result
+fillOnesInZerosInRow (x:xs) rowIndex counter result | x == 0 = fillOnesInZerosInRow xs rowIndex (counter+1) (setElem 1 (rowIndex, counter) result)
+													| otherwise = fillOnesInZerosInRow xs rowIndex (counter+1) result
 
 fill2IfMaxVals :: [Int] -> Int -> [Int] -> Int -> Matrix Int -> Matrix Int
 fill2IfMaxVals [] _ [] _ result = result
@@ -50,6 +91,15 @@ fill2IfMaxVals (y:ys) rowIndex xs colIndex result =
 -- zwraca liczbe '1' w liscie
 countOneVals :: [Int] -> Int
 countOneVals as = length (filter (==1) as)
+
+countZeroVals :: [Int] -> Int
+countZeroVals as = length (filter (==0) as)
+
+-- zlicza liczbę ze w sąsiedztwie włącznie z liczbą najbliższych wystapien 1
+countZeroValsAndNear1s :: [Int] -> Int -> Int
+countZeroValsAndNear1s [] startVal = startVal
+countZeroValsAndNear1s (a:as) startVal | a == 1 || a == 0 = countZeroValsAndNear1s as (startVal+1)
+									   | otherwise = countZeroValsAndNear1s as 0
 
 -- wstawia 2 jesli w wierszu jest juz konieczna ilosc jedynek
 fill2InCol :: Int -> [Int] -> Int -> Int -> Int -> Matrix Int -> Matrix Int
